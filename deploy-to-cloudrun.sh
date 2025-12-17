123221e349dc4bf8f1942c2baee7e0401dc0646c
Raven <raven39000@gmail.com>
2025-12-17 10:42:57 +0800

    fix sh

--- deploy-to-cloudrun.sh
+++ deploy-to-cloudrun.sh
@@ -105,12 +105,14 @@ else
     gcloud projects add-iam-policy-binding $PROJECT_ID \
         --member=serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com \
         --role=roles/run.admin \
+        --condition=None \
         --quiet
 
     gcloud iam service-accounts add-iam-policy-binding \
         ${PROJECT_NUMBER}-compute@developer.gserviceaccount.com \
         --member=serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com \
         --role=roles/iam.serviceAccountUser \
+        --condition=None \
         --quiet
 fi
 
