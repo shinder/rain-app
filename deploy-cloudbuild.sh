180c642e28b74692ac1536d27cbe8b6e481aa8fc
Shinder Lin <shinder.lin@gmail.com>
2025-12-18 15:41:06 +0800

    設定 cloud build

---
+++ deploy-cloudbuild.sh
@@ -0,0 +1,9 @@
+# 使用 Cloud Build 建立 image 和發佈
+
+gcloud builds submit
+
+# ------ 以下為停止服務 ----------
+# gcloud run services delete SERVICE_NAME \
+#   --region asia-east1
+
+# gcloud run services delete vue3-frontend --region europe-west1
